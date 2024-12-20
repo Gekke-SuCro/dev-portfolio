c0edfe7e35c9f81564b9333d0bf79b263f9f65cd
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-20 16:12:18 +0100

    feat: Created start for homepage in HomePage.jsx.
    fix: Fixed tailwindcss not working by adding correct content in tailwind.config.js and by adding the postcss.config.js file.

---
+++ frontend/src/views/HomePage.jsx
@@ -0,0 +1,14 @@
+import "../styles/HomePage.css"
+
+const HomePage = () => {
+    return (
+        <>
+            <div className="text-center my-[5rem]">
+                <h1 className="text-gray-900 text-5xl">Title</h1>
+                <p className="text-gray-900 text-xl">Short description</p>
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
