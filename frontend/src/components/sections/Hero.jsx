04632dc59e2d58512b54cb746bc0c15a7dff1a8a
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-21 14:01:41 +0100

    Updated contact me button in hero/ intro section.

--- frontend/src/components/sections/Hero.jsx
+++ frontend/src/components/sections/Hero.jsx
@@ -4,23 +4,29 @@ import ChevronSVG from "../../assets/img/chevron-down.svg"
 const Hero = ({ personaData }) => {
     return (
         <>
-            <section className="container min-h-svh text-center">
+            <section className="container h-svh text-center">
 
                 {/* Intro */}
-                <div className="flex flex-col items-center justify-center flex-grow">
+                <div className="flex flex-col sm:flex-row-reverse items-center justify-center flex-grow">
                     <img
                         className="w-40 mx-auto"
                         src={ProgrammerImg}
                         alt="Tech Boy"
                     />
-                    <h1 className="text-4xl font-bold">{personaData?.fullName}</h1>
-                    <p className="text-2xl font-medium">{personaData?.branche}</p>
-                    <button className="mt-6 py-2 px-4 rounded-md bg-gray-900 text-gray-200">Contact me</button>
+                    <div>
+                        <h1 className="text-4xl font-black">{personaData?.fullName}</h1>
+                        <p className="text-2xl font-bold">{personaData?.branche}</p>
+                        <button className="mt-6 py-2 px-4 rounded-md bg-accent text-white">Contact me</button>
+                    </div>
                 </div>
 
                 {/* Chevron */}
-                <div className="mb-6">
-                    <p className="font-light text-sm mb-6">Transforming ideas into software solutions</p>
+                <div>
+                    <p className="font-light text-sm mb-6">
+                        <span className="text-accent">&#34;</span>
+                        Transforming ideas into software solutions
+                        <span className="text-accent">&#34;</span>
+                    </p>
                     <img
                         className="w-12 h-12 mx-auto animate-bounce cursor-pointer"
                         src={ChevronSVG}
