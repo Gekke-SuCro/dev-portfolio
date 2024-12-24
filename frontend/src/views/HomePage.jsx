696f613a95f82a1322f2526ae86448e506eaeab8
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-24 13:46:42 +0100

    Commented structure in HomePage.jsx.

--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -3,10 +3,12 @@ import "../styles/HomePage.css"
 const HomePage = () => {
     return (
         <>
-            <div className="text-center my-[5rem]">
-                <h1 className="text-gray-900 text-5xl">Title</h1>
-                <p className="text-gray-900 text-xl">Short description</p>
-            </div>
+
+            {/* Navbar */}
+
+            {/* Main content */}
+
+            {/* Footer */}
         </>
     )
 }
