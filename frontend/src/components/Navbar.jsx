63e661d200d02dd81abc3015abd8f0123369f943
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-24 16:24:53 +0100

    Navbar.jsx now displays list of aligned menu-item-links.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
+import {Link} from "react-router-dom";
+
+const menuItems = [
+    { id: 1, name: "Home", link: "#home" },
+    { id: 2, name: "About", link: "#home" },
+    { id: 3, name: "Skills", link: "#skills" },
+]
+
 const Navbar = () => {
     return (
         <header className="px-8 py-6 bg-white fixed top-0 left-0 w-full z-50">
             <nav className="flex items-center justify-between">
-                <div>Logo</div>
-                <ul>Nav list</ul>
+                <div className="font-bold text-md">Logo</div>
+                <ul className="flex items-center justify-between gap-4">
+                    {menuItems.map((item) => (
+                        <li key={item.id}>
+                            <Link to={`/${item.link}`}>
+                                <h4>{item.name}</h4>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </nav>
         </header>
     )
