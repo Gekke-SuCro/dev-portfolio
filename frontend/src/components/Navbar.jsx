22206b59c8f5e78ff21a8782b8aa888fe58f9062
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-24 17:28:17 +0100

    Mobile menu is responsive for landscape mobile.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,8 @@ const menuItems = [
     { id: 1, name: "Home", link: "#home" },
     { id: 2, name: "About", link: "#home" },
     { id: 3, name: "Skills", link: "#skills" },
+    { id: 3, name: "Projects", link: "#projects" },
+    { id: 3, name: "Contact", link: "#contact" },
 ]
 
 const Navbar = () => {
@@ -15,19 +17,22 @@ const Navbar = () => {
         <header className="px-8 py-6 bg-white fixed top-0 left-0 w-full z-50">
             <nav className="flex items-center justify-between">
                 <div className="font-bold text-xl">Logo</div>
-                <div className={`${isOpen ? "left-1/3" : "left-full"} transition-all duration-2/00 ease-in 
-                bg-accent bg-opacity-90 fixed top-0 w-2/3 h-full`}>
-                    <ul className={`flex flex-col items-center justify-center gap-8 w-full h-full`}>
+                <div className={`${isOpen ? "left-1/3" : "left-[200svw]"} transition-all duration-2/00 ease-in 
+                bg-accent bg-opacity-90 fixed top-0 w-2/3 h-full md:relative md:left-0 md:bg-white`}>
+                    <ul className={`flex flex-col flex-wrap px-8 py-28 items-center justify-center gap-8 w-full h-full
+                    sm:flex-row`}>
                         {menuItems.map((item) => (
                             <li key={item.id}>
                                 <Link to={`/${item.link}`}>
-                                    <h4 className="text-xl text-white font-bold">{item.name}</h4>
+                                    <h4 className="font-poppins max-md:text-base max-md:text-white max-md:font-bold flex gap-2 items-center">
+                                        <span className="text-sm md:text-accent italic">0{item.id}</span> {item.name}
+                                    </h4>
                                 </Link>
                             </li>
                         ))}
                     </ul>
                 </div>
-                <div className={`z-50 ${isOpen ? "text-white" : "text-black"}`} onClick={() => setIsOpen(!isOpen)}>
+                <div className={`md:hidden z-50 ${isOpen ? "text-white" : "text-black"}`} onClick={() => setIsOpen(!isOpen)}>
                     {isOpen ? <RiMenu3Line size={40}/> : <RiMenuFill size={40}/>}
                 </div>
             </nav>
