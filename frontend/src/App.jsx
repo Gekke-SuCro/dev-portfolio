22e6d6d96a6d4147ff6ded3a6bae11cbdd3db677
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-24 13:42:38 +0100

    React router dom setup.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
 import HomePage from "./views/HomePage";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+        errorElement: <span>Oops! Something went wrong</span>
+    }
+])
 
 function App() {
-
-  return (
-    <>
-     <HomePage />
-    </>
-  )
+    return <RouterProvider router={router} />;
 }
 
 export default App
