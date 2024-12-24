87c79f28aeacebee9b0ea0b526e16a8f080eb86a
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-24 13:57:30 +0100

    HomePage.jsx contains structure for the page layout. Updated tailwind.config.js and index.css with placeholder font families and colors.

--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -3,12 +3,26 @@ import "../styles/HomePage.css"
 const HomePage = () => {
     return (
         <>
-
             {/* Navbar */}
+            <header>
+                <nav>
+                    Navbar
+                </nav>
+            </header>
 
             {/* Main content */}
+            <main>
+                {/* Hero */}
+                <section>
+                    <h1 className="font-poppins">Jayden Roeper</h1>
+                    <h2 className="font-sourceCodePro text-accent">Full-stack Developer</h2>
+                </section>
+            </main>
 
             {/* Footer */}
+            <footer>
+                Footer
+            </footer>
         </>
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        sourceCodePro: ['Source Code Pro', 'monospace'],
+      },
+      colors: {
+        black: '#000000',
+        white: '#ffffff',
+        accent: '#9abdba',
+      }
+    },
   },
   plugins: [],
 }
