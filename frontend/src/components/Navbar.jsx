3794fc448cb83c4a9def69960c1b92bb7a99a8fc
Gekke-SuCro <jayden.roeper@student.hu.nl>
2024-12-24 17:14:53 +0100

    Mobile toggle menu.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
+import  {useState} from "react";
 import {Link} from "react-router-dom";
+import { RiMenuFill, RiMenu3Line  } from "react-icons/ri";
 
 const menuItems = [
     { id: 1, name: "Home", link: "#home" },
@@ -7,19 +9,27 @@ const menuItems = [
 ]
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="px-8 py-6 bg-white fixed top-0 left-0 w-full z-50">
             <nav className="flex items-center justify-between">
-                <div className="font-bold text-md">Logo</div>
-                <ul className="flex items-center justify-between gap-4">
-                    {menuItems.map((item) => (
-                        <li key={item.id}>
-                            <Link to={`/${item.link}`}>
-                                <h4 className="nav-link">{item.name}</h4>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+                <div className="font-bold text-xl">Logo</div>
+                <div className={`${isOpen ? "left-1/3" : "left-full"} transition-all duration-2/00 ease-in 
+                bg-accent bg-opacity-90 fixed top-0 w-2/3 h-full`}>
+                    <ul className={`flex flex-col items-center justify-center gap-8 w-full h-full`}>
+                        {menuItems.map((item) => (
+                            <li key={item.id}>
+                                <Link to={`/${item.link}`}>
+                                    <h4 className="text-xl text-white font-bold">{item.name}</h4>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={`z-50 ${isOpen ? "text-white" : "text-black"}`} onClick={() => setIsOpen(!isOpen)}>
+                    {isOpen ? <RiMenu3Line size={40}/> : <RiMenuFill size={40}/>}
+                </div>
             </nav>
         </header>
     )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,12 +13,12 @@ export default {
         h1: ['36px', '1.2'],
         h2: ['28px', '1.3'],
         h3: ['28px', '1.4'],
-        nav: ['18px', '1.6'],
+        nav: ['20px', '1.6'],
       },
       colors: {
         black: '#000000',
         white: '#ffffff',
-        accent: '#9abdba',
+        accent: '#828f8f',
       }
     },
   },
